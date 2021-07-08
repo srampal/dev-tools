7f029c11459c5a9cb85cc89bb297c2e7f0d4c134
Sanjeev Rampal <srampal@redhat.com>
2021-07-08 08:42:34 -0400

    Latest ovn-k8s requires kind v0.11.1 .. updated ..

--- dev-vm/bootstrap.sh
+++ dev-vm/bootstrap.sh
@@ -70,7 +70,7 @@ function setup_fedora_vm() {
 	sudo systemctl enable libvirtd
 
 	#Install kind
-	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
+	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 	chmod +x ./kind
 	sudo mv ./kind /usr/local/bin
 
